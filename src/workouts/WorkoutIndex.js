f183f5fc05d5f80c9911b4405b7fe8f8add33add
Cindy Blahunka <cindyblahunka@gmail.com>
2021-07-01 18:37:57 -0400

    second commit

--- src/workouts/WorkoutIndex.js
+++ src/workouts/WorkoutIndex.js
@@ -1,22 +1,39 @@
 import React, {useState, useEffect} from 'react';
 import {Container, Row, Col} from 'reactstrap';
 import WorkoutCreate from './WorkoutCreate';
+import WorkoutTable from './WorkoutTable';
+import WorkoutEdit from './WorkoutEdit';
 
 const WorkoutIndex = (props) => {
     const [workouts, setWorkouts] = useState([]);
+    const [updateActive, setUpdateActive] = useState(false);
+    const [workoutToUpdate, setWorkoutToUpdate] = useState({});
 
     const fetchWorkouts = () => {
         fetch("http://localhost:3000/log", {
             method: 'GET', 
             headers: new Headers ({
                 'Content-Type': 'application/json',
-                'Authorization': props.token
+                'Authorization': `Bearer ${props.token}`
             })
         }).then(
             (response) => response.json()
         ).then((logData) => {
             setWorkouts(logData)
         })
+    }
+
+    const editUpdateWorkout = (workout) => {
+        setWorkoutToUpdate(workout);
+        console.log(workout);
+    }
+
+    const updateOn = () => {
+        setUpdateActive(true);
+    }
+
+    const updateOff = () => {
+        setUpdateActive(false);
     }
 
     useEffect(() => {
@@ -30,8 +47,11 @@ const WorkoutIndex = (props) => {
                     <WorkoutCreate fetchWorkouts={fetchWorkouts} token={props.token}/>
                 </Col>
                 <Col md="9">
-                    <h2>Log a workout to see a table.  This will be added in later pages.</h2>
+                    <WorkoutTable workouts={workouts} editUpdateWorkout={editUpdateWorkout} 
+                    updateOn={updateOn} fetchWorkouts={fetchWorkouts} token={props.token}/>
                 </Col>
+                {updateActive ? <WorkoutEdit workoutToUpdate={workoutToUpdate}
+                updateOff={updateOff} token={props.token} fetchWorkouts={fetchWorkouts}/> : <></>}
             </Row>
         </Container>
     );
